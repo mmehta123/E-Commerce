6202440359bafd093be6ab4a482bc78e83cd9828
Mohit Mehta <94632506+mmehta123@users.noreply.github.com>
2022-10-21 13:59:06 +0530

    admin panel

--- frontend/src/redux/cartRedux.js
+++ frontend/src/redux/cartRedux.js
@@ -10,24 +10,6 @@ const cartSlice = createSlice({
     },
     reducers: {              // 1.2  this objects containes all our actions means addProduct is an action
         addProduct: (state, action) => {
-            // Login to increase quantity of same product with same specifications(Not adding as another product) 
-            // if(state.products.length!==0){
-            //     state?.products.map(product=>{
-            //         //this will check if same product already exists with smae color and size if yes then it will only increse its quantity 
-            //         if (action.payload?.color === product?.color && action.payload?.size === product?.size &&
-            //             action.payload?._id === product?._id) {
-            //                     product.quantity+=action.payload.quantity;
-            //             }else{
-            //                 // else it will add it as new product
-            //                 state.products.push(action.payload);
-            //                 state.quantity += 1;      //this is not product's quantity it is no. of type of products we added to cart 
-            //             }
-            //         });
-            //     }else{
-            //         // if it is our first product then it appand it first time
-            //         state.products.push(action.payload);
-            //         state.quantity += 1;      //this is not product's quantity it is no. of type of products we added to cart 
-            //     }
             state.products.push(action.payload);
             state.quantity += 1; 
             state.totalPrice += action.payload.price * action.payload.quantity;
